6b947bc1e8e775bc3ecd8bb3474db805e3045cdb
learninginprogresss <amoslim90@gmail.com>
2022-09-11 19:38:55 +0800

    modified app.py

--- app.py
+++ app.py
@@ -28,7 +28,7 @@ def main_page():
         file.save(os.path.join('uploads', filename))
         return redirect(url_for('prediction', filename=filename))
     return render_template('index.html')
-
+    
 @app.route('/index')
 def index():
     return render_template('index.html')
@@ -40,7 +40,7 @@ def index2():
         filename = secure_filename(file.filename)
         file.save(os.path.join('uploads', filename))
         return redirect(url_for('prediction', filename=filename))
-    return render_template('index.html')
+    return render_template('index2.html')
 
 @app.route('/prediction/<filename>') 
 def prediction(filename):
